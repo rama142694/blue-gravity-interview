ac008d699124b6225f07aaa9eaef19c62b1da52e
Esposito Ramiro <ramiro142694@gmail.com>
2023-05-24 22:56:16 -0300

    remove unnecesary sprites

--- Assets/Scripts/Shopkeeper.cs
+++ Assets/Scripts/Shopkeeper.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class Shopkeeper : MonoBehaviour, IInteractuable
 {
     [SerializeField] private Store _store;
+    [SerializeField] private Player _player;
+    [SerializeField] private GameObject _interactionPopUp;
 
     private void Start()
     {
         _store.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, _player.transform.position) <= 3)
+        {
+            _interactionPopUp.gameObject.SetActive(true);
+        }
+        else
+        {
+            _interactionPopUp.gameObject.SetActive(false);
+        }
     }
 
     public void Interact()
